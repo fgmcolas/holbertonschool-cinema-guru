3f41d4494ebb38ba1a997d4fe9625beade4fe50c
fgmcolas <https://github.com/fgmcolas>
2025-02-21 17:12:17 +0100

    App modified for task 3

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from "axios";
+import Dashboard from "./components/dashboard/Dashboard";
+import Authentication from "./components/auth/Authentication";
 
 function App() {
-  return <div className="App"></div>;
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userUsername, setUserUsername] = useState("");
+
+  useEffect(() => {
+    const accessToken = localStorage.getItem("accessToken");
+
+    if (accessToken) {
+      axios
+        .post("/api/auth/", {}, { headers: { Authorization: `Bearer ${accessToken}` } })
+        .then((response) => {
+          setIsLoggedIn(true);
+          setUserUsername(response.data.username);
+        })
+        .catch((error) => {
+          console.error("Authentication failed:", error);
+          setIsLoggedIn(false);
+          setUserUsername("");
+        });
+    }
+  }, []);
+
+  return <div className="App">{isLoggedIn ? <Dashboard /> : <Authentication />}</div>;
 }
 
 export default App;
