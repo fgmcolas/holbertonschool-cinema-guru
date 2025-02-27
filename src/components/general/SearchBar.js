dd019558ca98810d8109ead7ffa0a1d5497bfb8f
fgmcolas <https://github.com/fgmcolas>
2025-02-27 17:10:24 +0100

    Improving searchbar focused properties

--- src/components/general/SearchBar.js
+++ src/components/general/SearchBar.js
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./general.css";
 
 const SearchBar = ({ title, setTitle }) => {
+    const [isFocused, setIsFocused] = useState(false);
+
     const handleInput = (event) => {
         setTitle(event.target.value);
     };
 
     return (
-        <div className="input-container">
-            <input type="text" placeholder="Search Movies" value={title} onChange={handleInput} />
+        <div className={`search-bar ${isFocused ? "focused" : ""}`}>
+            <input
+                type="text"
+                placeholder="Search Movies"
+                value={title}
+                onChange={handleInput}
+                onFocus={() => setIsFocused(true)}
+                onBlur={() => setIsFocused(false)}
+            />
         </div>
     );
 };
