1aa99b89e32e52d3ba0c14af7f9f59adabb21de4
fgmcolas <https://github.com/fgmcolas>
2025-02-27 20:30:32 +0100

    filter={true} added to tag

--- src/components/movies/Filter.js
+++ src/components/movies/Filter.js
@@ -14,14 +14,13 @@ const Filter = ({ minYear, setMinYear, maxYear, setMaxYear, sort, setSort, genre
     return (
         <div className="filter-container">
             <div className="filter-left">
-                <SearchBar title={title} setTitle={setTitle} className="search-bar" />
+                <SearchBar title={title} setTitle={setTitle} />
                 <div className="filters">
                     <Input label="Min Date:" type="number" value={minYear} setValue={setMinYear} className="filter-input" />
                     <Input label="Max Date:" type="number" value={maxYear} setValue={setMaxYear} className="filter-input" />
                     <SelectInput
                         label="Sort:"
                         options={[
-                            { value: "default", label: "Default" },
                             { value: "latest", label: "Latest" },
                             { value: "oldest", label: "Oldest" },
                             { value: "highestrated", label: "Highest Rated" },
@@ -33,11 +32,11 @@ const Filter = ({ minYear, setMinYear, maxYear, setMaxYear, sort, setSort, genre
                     />
                 </div>
             </div>
-            <div className="tags-container">
+            <ul className="tags-container">
                 {genresList.map((genre, index) => (
-                    <Tag key={index} genre={genre} genres={genres} setGenres={setGenres} />
+                    <Tag key={index} genre={genre} filter={true} genres={genres} setGenres={setGenres} />
                 ))}
-            </div>
+            </ul>
         </div>
     );
 };
