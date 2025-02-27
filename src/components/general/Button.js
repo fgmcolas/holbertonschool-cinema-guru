097740e191c3900536e43edced9bc9468ea52d57
fgmcolas <https://github.com/fgmcolas>
2025-02-27 19:45:02 +0100

    adding default value for classname

--- src/components/general/Button.js
+++ src/components/general/Button.js
@@ -2,9 +2,9 @@ import React from "react";
 import "./general.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const Button = ({ label, className, onClick, icon }) => {
+const Button = ({ label, className = "", onClick, icon }) => {
     return (
-        <button className={`button ${className}`} onClick={onClick}>
+        <button className={`button ${className || ""}`} onClick={onClick}>
             {icon && <FontAwesomeIcon icon={icon} className="button-icon" />}
             {label}
         </button>
